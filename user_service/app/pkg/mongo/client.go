050da05b0215a29c2627752eff8e046c63d574f6
juicyluv <juicyluv@mail.ru>
2022-02-07 23:55:57 +0300

    mongo database client

---
+++ user_service/app/pkg/mongo/client.go
@@ -0,0 +1,28 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// NewMongoClient tries to connect to database with provided options.
+// If everything is OK, returns a new mongo client instance.
+// Returns an error if something went wrong.
+func NewMongoClient(ctx context.Context, database, mongoURL string) (*mongo.Database, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL)
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to mongodb: %w", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not ping mongodb: %w", err)
+	}
+
+	return client.Database(database), nil
+}
