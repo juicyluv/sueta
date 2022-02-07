e188dda76a598c06db616a9cba85e26776226df2
juicyluv <juicyluv@mail.ru>
2022-02-07 13:39:13 +0300

    moved model and storage to the user folder

--- user_service/app/internal/storage.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-type Storage interface {
-	Create(ctx context.Context, user User) (string, error)
-	FindByEmail(ctx context.Context, email string) (User, error)
-	FindById(ctx context.Context, uuid string) (User, error)
-	Update(ctx context.Context, user User) error
-	Delete(ctx context.Context, uuid string) error
-}
