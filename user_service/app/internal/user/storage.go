4c18d550ca9d2f03ecb9cfe209137489a6eb936a
juicyluv <juicyluv@mail.ru>
2022-02-09 02:36:51 +0300

    fixed create function

--- user_service/app/internal/user/storage.go
+++ user_service/app/internal/user/storage.go
@@ -4,9 +4,9 @@ import "context"
 
 // Storage descibes a user storage functionality.
 type Storage interface {
-	Create(ctx context.Context, user *CreateUserDTO) (string, error)
+	Create(ctx context.Context, user *User) (string, error)
 	FindByEmail(ctx context.Context, email string) (*User, error)
 	FindById(ctx context.Context, uuid string) (*User, error)
-	UpdatePartially(ctx context.Context, user *UpdateUserDTO) error
+	UpdatePartially(ctx context.Context, user *User) error
 	Delete(ctx context.Context, uuid string) error
 }
