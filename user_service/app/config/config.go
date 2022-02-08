781bf6623b80b413764099d2964f29f636b9cf4d
juicyluv <juicyluv@mail.ru>
2022-02-08 17:17:40 +0300

    added comments for internal structures

--- user_service/app/config/config.go
+++ user_service/app/config/config.go
@@ -10,6 +10,7 @@ import (
 
 // Config describes an application configuration structure.
 type Config struct {
+	// Http represents configuration for http server.
 	Http struct {
 		Port           string `yaml:"port" env-default:"8080"`
 		MaxHeaderBytes int    `yaml:"maxHeaderBytes" env-default:"1"`
@@ -17,10 +18,12 @@ type Config struct {
 		WriteTimeout   int    `yaml:"writeTimeout" env-default:"20"`
 		RequestTimeout int    `yaml:"requestTimeout" env-default:"15"`
 	} `yaml:"http" env-required:"true"`
+	// DB represents configuration for database.
 	DB struct {
 		URL      string `env:"MONGO_URL" env-required:"true"`
 		Database string `yaml:"database" env-required:"true"`
 	} `yaml:"mongo" env-required:"true"`
+	// RedisDSN is DSN for redis connection.
 	RedisDSN string `env:"REDIS_DSN" env-required:"true"`
 }
 
