5b4c6726dd99db053ffb46419c74a52192bbd7ba
juicyluv <juicyluv@mail.ru>
2022-02-08 17:18:35 +0300

    added comments

--- user_service/app/internal/server/server.go
+++ user_service/app/internal/server/server.go
@@ -10,11 +10,13 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Server represents http server.
 type Server struct {
 	server *http.Server
 	logger *logger.Logger
 }
 
+// NewServer returns a new Server instance.
 func NewServer(cfg *config.Config, handler *httprouter.Router, logger *logger.Logger) *Server {
 	return &Server{
 		server: &http.Server{
@@ -28,10 +30,13 @@ func NewServer(cfg *config.Config, handler *httprouter.Router, logger *logger.Lo
 	}
 }
 
+// Run starts http server. Returns an error on failure.
 func (s *Server) Run() error {
 	return s.server.ListenAndServe()
 }
 
+// Shutdown closes all connections and shuts down http server.
+// It uses httpServer.Shutdown() function. Returns an error on failure.
 func (s *Server) Shutdown(ctx context.Context) error {
 	return s.server.Shutdown(ctx)
 }
