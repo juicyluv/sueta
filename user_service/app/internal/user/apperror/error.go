aeabd24164cf4336ea4f60567a79303ad161813c
juicyluv <juicyluv@mail.ru>
2022-02-08 16:40:42 +0300

    added comments and new errors

--- user_service/app/internal/user/apperror/error.go
+++ user_service/app/internal/user/apperror/error.go
@@ -1,21 +1,45 @@
 package apperror
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-var ()
+var (
+	// ErrNotFound is used when server needs to response with 404 Not Found status code.
+	ErrNotFound = NewAppError(
+		http.StatusNotFound,
+		"a given resource is not found",
+		"please, double check your request structure",
+	)
+)
 
 // AppError describes a structure of an error response in JSON format.
 type AppError struct {
 	Err              error  `json:"-"`
 	Message          string `json:"message,omitempty"`
 	DeveloperMessage string `json:"developerMessage,omitempty"`
+	HttpCode         int    `json:"code,omitempty"`
 }
 
 // NewAppError returns a new AppError instance.
-func NewAppError(message, developerMessage string) *AppError {
+func NewAppError(code int, message, developerMessage string) *AppError {
 	return &AppError{
 		Err:              fmt.Errorf(message),
 		Message:          message,
 		DeveloperMessage: developerMessage,
+		HttpCode:         code,
 	}
+}
+
+// BadRequestError returns a new AppError instance
+// with 400 Bad Request status code.
+func BadRequestError(message, developerMessage string) *AppError {
+	return NewAppError(http.StatusBadRequest, message, developerMessage)
+}
+
+// InternalError returns a new AppError instance
+// with 500 Internal Server Error status code.
+func InternalError(message, developerMessage string) *AppError {
+	return NewAppError(http.StatusInternalServerError, message, developerMessage)
 }
